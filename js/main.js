55b3b49530f7d70924de80906f5ce5e2e97dd404
Falkicon <Falkicon@users.noreply.github.com>
2025-03-06 09:19:09 +0000

    deploy: befb856c6a655627a0083bd2657f55204733459b

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // Main JavaScript file for Bloodfen Guides
 document.addEventListener('DOMContentLoaded', function() {
   console.log('Bloodfen Guides main.js loaded');
+  window.mainJsLoaded = true;
 }); 
